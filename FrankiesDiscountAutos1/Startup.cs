bbb02d46affbb9262c9134d4493461f75b440f70
stevieray8450 <stevieray8450@STEPHENBONID14B>
2017-05-27 00:14:42 -0500

    Add project files.

---
+++ FrankiesDiscountAutos1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrankiesDiscountAutos1.Startup))]
+namespace FrankiesDiscountAutos1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
